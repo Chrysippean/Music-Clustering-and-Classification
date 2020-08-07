b5555ff2e0c3c5d1bd1b07cde7f6a797576cbac5
Chrysippean <danielnnguyen16@yahoo.com>
2020-08-06 20:29:01 -0700

    updated report & source code

--- train.m
+++ train.m
@@ -1,12 +1,12 @@
 clear all;
 close all;
 clc;
-
-cd '/Users/daniel/MATLAB/AMATH 482/HW 3/Music'
-
+ 
+cd '/Users/daniel/MATLAB/Classifier/Music'
+ 
 musicfiles = dir(pwd);
 songname = dir('*wav');
-
+ 
 songs = [];
 sr = 44100; % sampling rate obtained from sampling first few songs
 for i = 3:length(musicfiles)
@@ -14,9 +14,9 @@ for i = 3:length(musicfiles)
     signal = Y(:,1) + Y(:,2);
     songs(:,i-2) = signal;
 end
-
+ 
 %%
-
+ 
 sgrams = zeros(146718,1000);
 window = hamming(1024);
 noverlap = 256;
@@ -27,9 +27,9 @@ for i = 1:length(songs(1,:))
     S = reshape(S, [spsize(1) * spsize(2), 1]);
     sgrams(:,i) = S;
 end
-
+ 
 %%
-
+ 
 blues = sgrams(:,1:100);
 classical = sgrams(:,101:200);
 country = sgrams(:,201:300);
@@ -40,9 +40,9 @@ metal = sgrams(:,601:700);
 pop = sgrams(:,701:800);
 reggae = sgrams(:,801:900);
 rock = sgrams(:,901:1000);
-
+ 
 %%
-
+ 
 [U1, S1, V1] = svd(blues,'econ');
 [U2, S2, V2] = svd(classical,'econ');
 [U3, S3, V3] = svd(country,'econ');
@@ -53,7 +53,7 @@ rock = sgrams(:,901:1000);
 [U8, S8, V8] = svd(pop,'econ');
 [U9, S9, V9] = svd(reggae,'econ');
 [U10, S10, V10] = svd(rock,'econ');
-
+ 
 %%
 figure(1)
 subplot(5,2,1)
@@ -76,13 +76,13 @@ subplot(5,2,9)
 plot(diag(S9),'ko')
 subplot(5,2,10)
 plot(diag(S10),'ko')
-
+ 
 %%
 mode_low = 1;
 mode_high = 10;
-
+ 
 %%
-
+ 
 figure(2)
 plot3(V1(:,mode_low),V1(:,mode_low+1),V1(:,mode_low+2),'o')
 hold on
@@ -104,18 +104,18 @@ plot3(V9(:,mode_low),V9(:,mode_low+1),V9(:,mode_low+2),'o')
 hold on
 plot3(V10(:,mode_low),V10(:,mode_low+1),V10(:,mode_low+2),'o')
 legend('blues','classical','country','disco','hiphop','jazz','metal','pop','reggae','rock');
-
+ 
 %% Training with Cross-Validation
-
+ 
 totalscores1 = [];
 totalscores2 = [];
 totalscores3 = [];
-
-
+ 
+ 
 c_v_iter = 5;
-
+ 
 for i = 1:c_v_iter
-
+ 
 q1 = randperm(100);
 q2 = randperm(100);
 q3 = randperm(100);
@@ -126,7 +126,7 @@ q7 = randperm(100);
 q8 = randperm(100);
 q9 = randperm(100);
 q10 = randperm(100);
-
+ 
 xblues = V1(:,mode_low:mode_high);
 xclassical = V2(:,mode_low:mode_high);
 xcountry = V3(:,mode_low:mode_high);
@@ -137,7 +137,7 @@ xmetal = V7(:,mode_low:mode_high);
 xpop = V8(:,mode_low:mode_high);
 xreggae = V9(:,mode_low:mode_high);
 xrock = V10(:,mode_low:mode_high);
-
+ 
 xtrain = [xblues(q1(1:70),:)
     xclassical(q2(1:70),:)
     xcountry(q3(1:70),:)
@@ -148,7 +148,7 @@ xtrain = [xblues(q1(1:70),:)
     xpop(q8(1:70),:)
     xreggae(q9(1:70),:)
     xrock(q10(1:70),:)];
-
+ 
 xtest = [xblues(q1(71:100),:)
     xclassical(q2(71:100),:)
     xcountry(q3(71:100),:)
@@ -159,9 +159,9 @@ xtest = [xblues(q1(71:100),:)
     xpop(q8(71:100),:)
     xreggae(q9(71:100),:)
     xrock(q10(71:100),:)];
-
+ 
 %%
-
+ 
 ctrain = [ones(70,1)
     2*ones(70,1)
     3*ones(70,1)
@@ -172,9 +172,9 @@ ctrain = [ones(70,1)
     8*ones(70,1)
     9*ones(70,1)
     10*ones(70,1)];
-
+ 
 %%
-
+ 
 ctrain3 = [ones(70,3)
     2*ones(70,3)
     3*ones(70,3)
@@ -185,12 +185,12 @@ ctrain3 = [ones(70,3)
     8*ones(70,3)
     9*ones(70,3)
     10*ones(70,3)];
-
+ 
 %% Naive Bayes
-
+ 
 nb = fitcnb(xtrain,ctrain);
 pre1 = nb.predict(xtest);
-
+ 
 bar(pre1)
 score = 0;
 scores1 = zeros(1,10);
@@ -206,9 +206,9 @@ for i = 1:length(pre1)
         scores1(floor(i/30)) = score / 30.0;
     end
 end
-
+ 
 %% Linear Discriminant Analysis
-
+ 
 pre2 = classify(xtest,xtrain,ctrain);
 bar(pre2)
 score = 0;
@@ -225,9 +225,9 @@ for i = 1:length(pre2)
         scores2(floor(i/30)) = score / 30.0;
     end
 end
-
+ 
 %% k-nearest neighbors
-
+ 
 [IDX, D] = knnsearch(xtrain,xtest);
 pre3=[];
 for i=1:length(xtest(:,1))
@@ -247,13 +247,13 @@ for i = 1:length(pre3)
         scores3(floor(i/30)) = score / 30.0;
     end
 end
-
+ 
 totalscores1 = [totalscores1; scores1];
 totalscores2 = [totalscores2; scores2];
 totalscores3 = [totalscores3; scores3];
-
+ 
 end
-
+ 
 averagescores1 = [];
 averagescores2 = [];
 averagescores3 = [];
@@ -262,19 +262,19 @@ for i = 1:length(scores1)
     averagescores2(:,i) = sum(totalscores2(:,i)) / c_v_iter;
     averagescores3(:,i) = sum(totalscores3(:,i)) / c_v_iter;
 end
-
+ 
 %%
-
+ 
 genrenames = {'blues','classical','country','disco','hiphop','jazz','metal','pop','reggae','rock'};
-
+ 
 tablem1 = [totalscores1; averagescores1].';
 tablem2 = [totalscores2; averagescores2].';
 tablem3 = [totalscores3; averagescores3].';
-
+ 
 table1 = table(tablem1(:,1), tablem1(:,2), tablem1(:,3), tablem1(:,4), tablem1(:,5), tablem1(:,6));
 table2 = table(tablem2(:,1), tablem2(:,2), tablem2(:,3), tablem2(:,4), tablem2(:,5), tablem2(:,6));
 table3 = table(tablem3(:,1), tablem3(:,2), tablem3(:,3), tablem3(:,4), tablem3(:,5), tablem3(:,6));
-
+ 
 table1.Properties.VariableNames = {'Trial1' 'Trial2' 'Trial3' 'Trial4' 'Trial5' 'Average'};
 table2.Properties.VariableNames = {'Trial1' 'Trial2' 'Trial3' 'Trial4' 'Trial5' 'Average'};
 table3.Properties.VariableNames = {'Trial1' 'Trial2' 'Trial3' 'Trial4' 'Trial5' 'Average'};
